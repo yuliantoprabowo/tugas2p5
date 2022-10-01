497ec48cc8d473811463ac9d3e8aec84db02e359
yuliantoprabowo <yuliantoprabowo23@gmail.com>
2022-10-01 15:43:14 +0700

    Initial commit

---
+++ src/tugas2pertemuan5/Pesan.java
@@ -0,0 +1,28 @@
+package tugas2pertemuan5;
+public class Pesan {
+    String nama,gula;
+    Integer harga,qty;
+
+    public Pesan(String nama, String gula, Integer harga, Integer qty) {
+        this.nama = nama;
+        this.gula = gula;
+        this.harga = harga;
+        this.qty = qty;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getGula() {
+        return gula;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+}
